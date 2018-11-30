7ac71d32ddabc4dad6d8f392928fb02b36620a9b
xterminal86 <xterminal86@gmail.com>
2018-11-30 16:22:15 +0300

    dumb compile script fixed

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sources=$(find src/ -iname "*.cpp");
-inc_ls=$(find inc/ -mindepth 1 -maxdepth 1 -type d)
-total_inc="";
-for d in ${inc_ls[@]};
-do
-  total_inc+="-iquote $d ";
-done
-g++ -O3 -Wall -mcmodel=medium $sources $total_inc -o nrogue -lcurses && ./nrogue
+cd build/
+rm -rf *
+cmake3 ../
+make -j4
+./nrogue
+
