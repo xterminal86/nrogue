f8f71012cef6233c8f79db80b78721605c9a1b31
xterminal86 <xterminal86@gmail.com>
2019-02-20 00:16:58 +0300

    time log numbers digit length fixed

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@
 int main(int argc, char* argv[])
 {
   RNG::Instance().Init();
-  RNG::Instance().SetSeed(1547020537474375167);
+  //RNG::Instance().SetSeed(1547020537474375167);
   //RNG::Instance().SetSeed(1);
   //RNG::Instance().SetSeed(1544714037606745311);
   //RNG::Instance().SetSeed(1545127588351497486);
--- src/singletons/logger.h
+++ src/singletons/logger.h
@@ -41,8 +41,25 @@ class Logger : public Singleton<Logger>
         time_t now = time(nullptr);
         tm *ltm = localtime(&now);
 
+        if (ltm->tm_hour >= 0 && ltm->tm_hour < 10)
+        {
+          _logFile << "0";
+        }
+
         _logFile << ltm->tm_hour << ":";
+
+        if (ltm->tm_min >= 0 && ltm->tm_min < 10)
+        {
+          _logFile << "0";
+        }
+
         _logFile << ltm->tm_min << ":";
+
+        if (ltm->tm_sec >= 0 && ltm->tm_sec < 10)
+        {
+          _logFile << "0";
+        }
+
         _logFile << ltm->tm_sec << " *** ";
 
         _logFile << stringToPrint << "\n";
