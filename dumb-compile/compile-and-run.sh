4a936643161a20df656dea4f5fd3a9aa136fdf4f
xterminal86 <xterminal86@gmail.com>
2020-01-21 10:49:36 +0300

    dumb compile script fixed a bit

--- dumb-compile/compile-and-run.sh
+++ dumb-compile/compile-and-run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+  echo "$0 <BUILD_TYPE>";
+  echo "<BUILD_TYPE>: \"Debug\" or \"Release\"";
+  exit 1;
+fi
+
 build_dir="build";
+build_type=$1;
+
+if [ $build_type != "Debug" -a $build_type != "Release" ]; then
+  echo "Wrong <BUILD_TYPE>: should be either \"Debug\" or \"Release\"";
+  exit 1;
+fi
 
 if [ ! -d $build_dir ]; then
   mkdir $build_dir
@@ -14,7 +26,7 @@ if [ $? -ne 0 ]; then
 fi
 
 rm -rf *
-cmake -D USE_SDL=ON -D CMAKE_BUILD_TYPE=Release ../../ || cmake3 -D USE_SDL=ON -D CMAKE_BUILD_TYPE=Release ../../
+cmake -D USE_SDL=ON -D CMAKE_BUILD_TYPE=$build_type ../../ || cmake3 -D USE_SDL=ON -D CMAKE_BUILD_TYPE=$build_type ../../
 make -j4
 ./nrogue
 
