6fdc3d7d3fad5967f1b35a246840f572cb1dc144
xterminal86 <xterminal86@gmail.com>
2022-05-04 13:03:05 +0300

    [mod] - tests are not compiled in if not enabled - dumb compile small refactoring

--- dumb-compile/compile-and-run.sh
+++ dumb-compile/compile-and-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 display_usage() {
-  echo "$0 <BUILD_TYPE> <CMAKE_JOBS=1> <SHOULD_RUN=0>";
+  echo "$0 <BUILD_TYPE> <CMAKE_JOBS=1> <USE_SDL=1> <SHOULD_RUN=0>";
   echo "<BUILD_TYPE>: \"Debug\" or \"Release\"";
 }
 
@@ -13,14 +13,22 @@ fi
 build_dir="build";
 build_type=$1;
 cmake_jobs=$2;
-should_run=$3;
+use_sdl=$3;
+should_run=$4;
 
-if [ -z "$cmake_jobs" ]; then
+if [ -z "$cmake_jobs" ];
+then
   cmake_jobs=1;
 fi
 
-if [ -z "$should_run" ]; then
+if [ -z "$should_run" ];
+then
   should_run=0;
+fi
+
+if [ -z "$use_sdl" ];
+then
+  use_sdl=1;
 fi
 
 if [ $build_type != "Debug" -a $build_type != "Release" ]; then
@@ -28,15 +36,21 @@ if [ $build_type != "Debug" -a $build_type != "Release" ]; then
   exit 1;
 fi
 
-if [ ! -d $build_dir ]; then
-  mkdir $build_dir
+if [ $use_sdl != "1" -a $use_sdl != "0" ];
+then
+  echo "<USE_SDL> should be 0 or 1";
+  exit 1;
 fi
 
-cd $build_dir
+if [ $use_sdl -eq 1 ]
+then
+  use_sdl="USE_SDL=ON";
+else
+  use_sdl="USE_SDL=OFF";
+fi;
 
-if [ $? -ne 0 ]; then
-  echo "Cannot cd into build directory!"
-  exit 1;
+if [ ! -d $build_dir ]; then
+  mkdir $build_dir
 fi
 
 cmake_command="cmake";
@@ -52,8 +66,22 @@ then
   exit 1;
 fi
 
-rm -rf *
-$cmake_command -D USE_SDL=ON -D CMAKE_BUILD_TYPE=$build_type ../../ && make -j$cmake_jobs
+rm -rf ${build_dir}
+
+prepare="${cmake_command} -B ${build_dir} -D ${use_sdl} -D CMAKE_BUILD_TYPE=${build_type} ../";
+
+echo ${prepare}
+
+if [ $should_run -eq 1 ]
+then
+  echo "(will autorun after compiling)";
+fi
+
+eval ${prepare}
+
+cmd="${cmake_command} --build ${build_dir} -j ${cmake_jobs}";
+
+eval ${cmd}
 
 if [ $should_run -ne 0 ]; then
   ./nrogue
